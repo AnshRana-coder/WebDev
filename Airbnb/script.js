a4321bd17ac56d2c88efe206068af8a9fe141c8f
AnshRana-coder <Anshjatrana555@gmail.com>
2025-07-01 13:08:46 +0530

    Header completed

--- Airbnb/script.js
+++ Airbnb/script.js
@@ -1,18 +1,13 @@
 document.addEventListener("DOMContentLoaded", function () {
   const searchBar = document.getElementById("searchBar");
   const middle = document.getElementById("middleSection");
-
-
+  const mainHeader = document.querySelector(".main-header");
 
   window.addEventListener("scroll", () => {
-    if (window.scrollY > 10) {
-      searchBar.classList.add("shrink");
-      middle.classList.add("hide");
-      
-    } else {
-      searchBar.classList.remove("shrink");
-      middle.classList.remove("hide");
+    const scrolled = window.scrollY > 10;
 
-    }
+    mainHeader.classList.toggle("scrolled", scrolled);   // shrink header
+    searchBar.classList.toggle("shrink", scrolled);      // shrink and move search bar
+    middle.classList.toggle("hide", scrolled);           // hide middle nav
   });
 });
